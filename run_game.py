ffaa37e25ec0ad898a7c75122a5b10c6e453d2f3
ap-ple <55882424+ap-ple@users.noreply.github.com>
2023-03-30 00:07:23 -0400

    Add files via upload

--- run_game.py
+++ run_game.py
@@ -1,9 +1,10 @@
 import sys
 from main import main
 
-MIN_VER = (3, 10)
+MIN_VER = (3, 11)
 
 if sys.version_info[:2] < MIN_VER:
-  sys.exit("This game requires Python {}.{}.".format(*MIN_VER))
+  input("This game requires Python {}.{}. Press ENTER to exit...".format(*MIN_VER))
+  sys.exit()
 else:
   main()
