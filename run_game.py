27bcef450cbe8c58df443eeb34fb9f43803c3b4b
ap-ple <55882424+ap-ple@users.noreply.github.com>
2023-03-30 00:28:09 -0400

    Add files via upload

--- run_game.py
+++ run_game.py
@@ -1,10 +1,15 @@
 import sys
-from main import main
 
 MIN_VER = (3, 11)
+
+try:
+  import main
+except ImportError:
+  input("This game requires Pygame. Use `pip install pygame` to install it. If this error persists, the game folder might be missing some files. In this case, try redownloading the game. Press ENTER to exit...".format(*MIN_VER))
+  sys.exit()
 
 if sys.version_info[:2] < MIN_VER:
   input("This game requires Python {}.{}. Press ENTER to exit...".format(*MIN_VER))
   sys.exit()
 else:
-  main()
+  main.main()
